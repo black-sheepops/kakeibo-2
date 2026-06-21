8b74d546d5dc8eb78593c92764289ed8f9950d98
black-sheep <black-sheep@example.com>
2026-06-21 15:44:57 +0900

    40th

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,8 @@ export default function RootLayout({
       <head>
         <meta name="theme-color" content="#10b981" />
         <meta name="mobile-web-app-capable" content="yes" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/myicon.png" /> {/* これがSafariのホーム画面用アイコン */}
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
       </head>
